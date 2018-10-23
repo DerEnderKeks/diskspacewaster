e3c096ae3be5eb80457986027fd98c67c618196f
DerEnderKeks <derenderkeks@gmail.com>
2018-10-23 21:57:07 +0200

    Debug output enhancement

--- main.go
+++ main.go
@@ -12,25 +12,26 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println("deleting...")
 	_, err := os.Stat(*path)
 	if os.IsExist(err) {
+		fmt.Println("Deleting old file...")
 		err := os.Remove(*path)
 		check(err)
+		fmt.Println("Deleted.")
 	}
-	fmt.Println("deleted.")
 
 	f, err := os.Create(*path)
 	defer f.Close()
 	check(err)
 
-	fmt.Println("writing...")
+	fmt.Printf("Creating new file at '%s' (Size: %d MB)...\n", *path, *size)
 	err = f.Truncate(int64(*size) * 1048576)
 	check(err)
 
-	fmt.Println("syncing...")
+	fmt.Println("Syncing...")
 	f.Sync()
-	fmt.Println("synced.")
+	fmt.Println("Synced.")
+	fmt.Printf("Done! Have fun with your new %d MB big file.\n", *size)
 }
 
 func check(err error) {
